e84525def5751030e3a6b34b1140c408ad9ac542
swatisam29 <swathi.sampathkumar29@gmail.com>
2023-07-16 01:06:08 -0500

    commit

--- lib/lib/model/constant.dart
+++
@@ -1,8 +0,0 @@
-class Constant {
-  static const devMode = true;
-  static const photoFileFolder = 'photo_files';
-  static const photoMemoCollection = 'photomemo_collection';
-  static const photoFavoriteFolder = 'photofav_folder';
-}
-
-enum ArgKey { user, downloadURL, filename, photoMemoList, onePhotoMemo }
